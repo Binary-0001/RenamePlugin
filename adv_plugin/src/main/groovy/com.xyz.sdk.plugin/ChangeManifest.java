e8f4a89fcd03e82edf8106c500570ee812048ece
zhangguoliang <zhangguoliang@021.com>
2021-05-20 19:09:01 +0800

    处理任务不同步问题

--- adv_plugin/src/main/groovy/com.xyz.sdk.plugin/ChangeManifest.java
+++ adv_plugin/src/main/groovy/com.xyz.sdk.plugin/ChangeManifest.java
@@ -3,8 +3,10 @@ package com.xyz.sdk.plugin;
 import com.android.build.gradle.AppExtension;
 import com.android.build.gradle.tasks.ManifestProcessorTask;
 
+import org.gradle.api.Action;
 import org.gradle.api.Project;
 import org.gradle.api.ProjectConfigurationException;
+import org.gradle.api.Task;
 import org.gradle.api.tasks.TaskInputs;
 
 import java.io.File;
@@ -40,15 +42,17 @@ public class ChangeManifest {
                 name = name.substring(0, 1).toUpperCase() + name.substring(1);
                 // 找到 形如 processReleaseManifest 的 task
                 ManifestProcessorTask manifestTask = (ManifestProcessorTask) project1.getTasks().getByName("process" + name + "Manifest");
-
-                manifestTask.doLast(task -> {
-                    try {
-                        System.out.println("--------------- RenamePlugin EditManifest: start ---------------");
-                        ManifestHelper helper = new ManifestHelper(manifestTask);
-                        helper.replace();
-                        System.out.println("--------------- RenamePlugin EditManifest: end ---------------");
-                    } catch (Exception e) {
-                        throw new RuntimeException("处理 Manifest 失败", e.getCause());
+                manifestTask.doLast(new Action<Task>() {
+                    @Override
+                    public void execute(Task task) {
+                        try {
+                            System.out.println("--------------- RenamePlugin EditManifest: start ---------------");
+                            ManifestHelper helper = new ManifestHelper(manifestTask);
+                            helper.replace();
+                            System.out.println("--------------- RenamePlugin EditManifest: end ---------------");
+                        } catch (Exception e) {
+                            throw new RuntimeException("处理 Manifest 失败", e.getCause());
+                        }
                     }
                 });
             });
